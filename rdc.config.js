8c6319794669a3058e921deb3235863a0ac0f83e
wangsong <wangsong3635@outlook.com>
2019-05-22 20:38:31 +0800

    update

--- rdc.config.js
+++ rdc.config.js
@@ -1,33 +1,42 @@
 module.exports = {
-    framework: 'vue',
-    docs: {
-        url: 'https://rdepro.github.io/rdc-vue-starter/'
-    },
-    render: {
-        includes: ['.html', '.js'],
-        mock: {
-            title: 'Hello World'
-        },
-        dev: {
-            render: {
-                suites: [
-                    {
-                        name: 'sc-common-vue',
-                        version: '1.0',
-                        alias: 'scCommonVue'
-                    }
-                ],
-            }
-        }
-    },
-    mappings: [
-        { from: 'app', to: 'src' },
+  framework: 'vue',
+  docs: {
+    url: 'https://rdepro.github.io/rdc-nut/'
+  },
+  render: {
+    includes: [
+      '.html',
+      '.js'
     ],
-    docker: {
-        tag: 'rdebase/rdc-vue-starter:0.0.1-alpha.6',
-        ports: ['8080:8080']
-    },
-    lint: {
-        files: ['*.js', '*.vue']
+    dev: {
+      render: {
+        suites: [
+          {
+            name: 'sc-common-vue',
+            version: '1.0',
+            alias: 'scCommonVue'
+          }
+        ],
+        title: 'Hello World'
+      }
     }
-};
+  },
+  mappings: [
+    {
+      from: 'app',
+      to: 'src'
+    }
+  ],
+  docker: {
+    tag: 'rdebase/rdc-nut:0.0.1-alpha.1',
+    ports: [
+      '8080:8080'
+    ]
+  },
+  lint: {
+    files: [
+      '*.js',
+      '*.vue'
+    ]
+  }
+}
