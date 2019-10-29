328db2c11b763258c9e4710764706550d9e92bb8
wangsong <wangsong3635@outlook.com>
2019-10-29 19:00:59 +0800

    feat: update rds-vue ks-select limit

--- rdc.config.js
+++ rdc.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     npm: {
         name: 'rdc-nut',
-        version: '0.0.1-beta.1',
+        version: '0.0.1-beta.3',
     },
     lint: {
         ext: ['.js', '.vue', '.ts']
