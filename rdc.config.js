d6bd20852ee88d3c917e3464b72e818d5ef4cbec
hzhayue <hzhayue@corp.netease.com>
2019-11-13 21:21:36 +0800

    chore: update scm-layout

--- rdc.config.js
+++ rdc.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     npm: {
         name: 'rdc-nut',
-        version: '0.0.1-beta.5',
+        version: '0.0.1-beta.6',
     },
     lint: {
         ext: ['.js', '.vue', '.ts']
