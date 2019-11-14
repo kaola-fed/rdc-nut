405178be6e53aa0ba63104581c1c4d1b0762402c
wangsong <wangsong3635@outlook.com>
2019-11-14 12:59:10 +0800

    chore: update scm-layout

--- rdc.config.js
+++ rdc.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     npm: {
         name: 'rdc-nut',
-        version: '0.0.1-beta.8',
+        version: '0.0.1-beta.9',
     },
     lint: {
         ext: ['.js', '.vue', '.ts']
