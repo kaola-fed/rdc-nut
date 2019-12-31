c24fb6e1c9093369e55d0a4a739633b3ee05607c
hzhayue <hzhayue@corp.netease.com>
2019-12-31 14:28:03 +0800

    chore: using ali env

--- rdc.config.js
+++ rdc.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     npm: {
         name: 'rdc-nut',
-        version: '0.0.1-beta.28',
+        version: '0.0.1-beta.29',
     },
     lint: {
         ext: ['.js', '.vue', '.ts']
