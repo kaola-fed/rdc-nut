a3c6d3d70ca90575ec8e8921b6f58ab48075fe28
soonwang <wangsong3635@outlook.com>
2020-01-01 11:18:43 +0800

    feat: beta.31

--- rdc.config.js
+++ rdc.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     npm: {
         name: 'rdc-nut',
-        version: '0.0.1-beta.30',
+        version: '0.0.1-beta.31',
     },
     lint: {
         ext: ['.js', '.vue', '.ts']
