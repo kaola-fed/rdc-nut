580b65ca61b0448d3903db53babae03669e25966
hzhayue <hzhayue@corp.netease.com>
2020-01-13 16:17:05 +0800

    chore: 升级nekui

--- rdc.config.js
+++ rdc.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     npm: {
         name: 'rdc-nut',
-        version: '0.0.1-beta.33',
+        version: '0.0.1-beta.34',
     },
     lint: {
         ext: ['.js', '.vue', '.ts']
