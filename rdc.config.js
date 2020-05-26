8628201605ce4240c8edbc1d2de56411a8d40697
易琦 <yiqi.hl@alibaba-inc.com>
2020-05-26 12:28:54 +0800

    feat: 更新layout，并锁死版本

--- rdc.config.js
+++ rdc.config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     npm: {
         name: 'rdc-nut',
-        version: '0.0.1-beta.53',
+        version: '0.0.1-beta.54',
     },
     lint: {
         ext: ['.js', '.vue', '.ts']
